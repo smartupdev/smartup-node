4e579deba432c1902ba8259324b55a1fbc87f043
zhangzhen <zhangzhen@xiejiatech.com>
2019-05-09 16:28:01 +0800

    add beta env

--- src/main/java/global/smartup/node/SchedulingConfig.java
+++ src/main/java/global/smartup/node/SchedulingConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Profile({"dev", "test", "prod"})
+@Profile({"dev", "test", "beta", "prod"})
 @EnableScheduling
 @Configuration
 public class SchedulingConfig {
