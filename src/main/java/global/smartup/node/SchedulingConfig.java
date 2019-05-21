78fc2aa7dfb92f7216e44e2e6671709ee41adf64
ZhangZhen-SmartUp <zhen.zhang@smartup.global>
2019-05-21 15:26:07 +0800

    fix university env has not parse block job

--- src/main/java/global/smartup/node/SchedulingConfig.java
+++ src/main/java/global/smartup/node/SchedulingConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@Profile({"dev", "test", "beta", "prod"})
+@Profile({"dev", "test", "beta", "university", "prod"})
 @EnableScheduling
 @Configuration
 public class SchedulingConfig {
